5dc5a8a8fded73b43f960e3c600d6a32b77d17ea
Paul Han <paul.myung.han@gmail.com>
2020-08-06 16:30:54 -0700

    created the recipe list component template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model"; 
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is a test", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
