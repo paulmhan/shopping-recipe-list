77fbc74256121346913f8936d81ef63c63353304
Paul Han <paul.myung.han@gmail.com>
2020-08-20 15:54:55 -0700

    can choose individual recipe for details

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe.model"; 
 
 @Component({
@@ -8,6 +8,7 @@ import { Recipe } from "../recipe.model";
 })
 
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe("Test Recipe", "This is a test", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
   ];
@@ -15,6 +16,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
