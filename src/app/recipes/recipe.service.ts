acaab346597872cfab18a0506f2d25f84af4d41c
Paul Han <paul.myung.han@gmail.com>
2020-10-15 10:52:16 -0700

    used services to allow user to send ingredients to shoppin list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,10 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
-
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
@@ -27,9 +28,17 @@ export class RecipeService {
             ])
     ];
 
+    constructor(private slService: ShoppingListService){
+
+    }
+
     getRecipes(){
         //slice only gives us a copy of the recipes
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients:Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 
     
