26dbce2712436cea3f36e099a3ffacad4d688ef9
Paul Han <paul.myung.han@gmail.com>
2020-10-15 09:48:23 -0700

    added ingredients to recipe model and now displays when recipe clicks

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("Test Recipe", "This is a test", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg"),
-        new Recipe("2 Test Recipe", "This is a test", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
+        new Recipe(
+            "Tasty Schnitzel", 
+            "A super tasty Schnitzel - just awesome!", 
+            "https://www.curiouscuisiniere.com/wp-content/uploads/2060/09/German-Pork-Schnitzel-9121-1200.jpg",
+            [
+                new Ingredient("Meat",1),
+                new Ingredient("French Fries", 20)
+            ]),
+        new Recipe(
+            "Big Fat Burger", 
+            "What else do you need to say?", 
+            "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/6/1/2/FNM_070112-Copy-That-Almost-Famous-Animal-Style-Burger-Recipe_s4x3.jpg.rend.hgtvcom.406.305.suffix/1382541460839.jpeg",
+            [
+                new Ingredient("Meat",1),
+                new Ingredient("French Fries", 20),
+                new Ingredient("Buns", 2),
+                new Ingredient("Cheese", 1),
+            ])
     ];
 
     getRecipes(){
