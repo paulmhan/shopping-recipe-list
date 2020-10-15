b7547744237e8f187f057835118bce6808799ff5
Paul Han <paul.myung.han@gmail.com>
2020-10-15 09:14:31 -0700

    using service for cross component comm

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe("Test Recipe", "This is a test", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg"),
         new Recipe("2 Test Recipe", "This is a test", "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
@@ -10,5 +14,7 @@ export class RecipeService {
         //slice only gives us a copy of the recipes
         return this.recipes.slice();
     }
+
+    
 }
 
