2a253469988c5c6587bb1e85fd899f5b3b61b36c
Paul Han <paul.myung.han@gmail.com>
2020-10-16 16:51:03 -0700

    configured route parameters

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-details',
@@ -8,12 +9,22 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
   
-  constructor(private reciperService: RecipeService) { }
+  constructor(private reciperService: RecipeService,
+    private route: ActivatedRoute) { 
+
+    }
 
   ngOnInit(): void {
-
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params["id"];
+          this.recipe=this.reciperService.getRecipe(this.id);
+        }
+      )
   }
 
   onAddToShoppingList(){
