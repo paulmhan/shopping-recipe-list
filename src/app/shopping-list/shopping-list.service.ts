d1b5253c7bee279d51df5cdb281649755b81d9c8
Paul Han <paul.myung.han@gmail.com>
2020-10-20 16:29:47 -0700

    load item info into input fields

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,11 +4,16 @@ import { Subject } from "rxjs";
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
+
+  getIngredient(index:number){
+    return this.ingredients[index];
+  }
 
   getIngredients() {
     return this.ingredients.slice();
