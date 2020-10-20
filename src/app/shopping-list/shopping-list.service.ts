cf89dd363e51f4e0df209de84ddab20c8b2f77d1
Paul Han <paul.myung.han@gmail.com>
2020-10-20 16:33:16 -0700

    created update item function that allows user to update ingredient without deleting

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -19,6 +19,11 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  updateIngredient(index:number, newIngredient: Ingredient){
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
