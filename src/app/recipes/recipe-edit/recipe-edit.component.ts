608a559fcee18ac379a6af3ed97fd73af2bfdf26
Paul Han <paul.myung.han@gmail.com>
2020-10-22 11:40:09 -0700

    created getting function to fit ngfor loop

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -95,4 +95,8 @@ export class RecipeEditComponent implements OnInit {
       ingredients: recipeIngredients
     });
   }
+
+  get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 }
