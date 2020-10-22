694a33b50b71ba4d13f9206425944860e07a9261
Paul Han <paul.myung.han@gmail.com>
2020-10-22 10:52:44 -0700

    make sure recipe has ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: "root" })
 
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
 
-    storeRecipes(){
+    storeRecipes() {
         const recipes = this.recipeService.getRecipes();
         this.http.put("https://ng-recipe-book-58248.firebaseio.com/recipes.json", recipes)
             .subscribe(response => {
@@ -16,11 +17,16 @@ export class DataStorageService {
             });
     }
 
-    fetchRecipes(){
+    fetchRecipes() {
         this.http.get<Recipe[]>("https://ng-recipe-book-58248.firebaseio.com/recipes.json")
-            .subscribe(recipes=>{
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                })
+            }))
+            .subscribe(recipes => {
                 console.log(recipes);
                 this.recipeService.setRecipes(recipes);
-        })
+            })
     }
 }
