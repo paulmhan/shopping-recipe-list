7d0c311ab4088eafb9e0ad1575cabe2717e6ea8f
Paul Han <paul.myung.han@gmail.com>
2020-10-23 17:31:22 -0700

    imported loading spinner

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+
 export interface AuthResponseData {
     kind: string;
     idToken: string;
@@ -13,7 +14,7 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: "root"})
 
-export class AuthServicee {
+export class AuthService {
 
     constructor(private http: HttpClient){
 
